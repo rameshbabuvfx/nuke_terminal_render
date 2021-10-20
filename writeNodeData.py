4c8fd24309ec1a0ce661bb9ef1bf10196b878c6b
rameshbabuvfx <rameshkanna95@gmail.com>
2021-10-21 00:14:59 +0530

    Done

--- writeNodeData.py
+++ writeNodeData.py
@@ -4,4 +4,10 @@ import nuke
 nuke.scriptOpen(sys.argv[1])
 writeNodes = nuke.allNodes("Write")
 for node in writeNodes:
-    print(node.name(), ":", node.frameRange())
+    if nuke.NUKE_VERSION_MAJOR > 12:
+        print(str(node.name()), ":", str(node.frameRange()))
+    else:
+        a_set = str(node.name()), ":", str(node.frameRange())
+        list_of_strings = [str(s) for s in a_set]
+        joined_string = " ".join(list_of_strings)
+        print(str(joined_string))
